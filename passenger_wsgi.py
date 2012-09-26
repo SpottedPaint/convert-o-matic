3115541934fd7cf5eed79669197c54219f68c411
Adam <derangedofarkham@yahoo.co.uk>
2012-09-26 14:31:44 +0100

    adding proper content

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,16 @@
 """
 Passenger WSGI script for hosting on Dreamhost. Assumes a virtualenv
-configured at ~/python/env.
 """
 import sys, os
 INTERP = os.path.join(os.environ['HOME'], 'flask_env', 'bin', 'python')
 if sys.executable != INTERP:
 	os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
-def application(environ, start_response):
-	start_response('200 OK', [('Content-type', 'text/plain')])
-	return ["Hello, world! Passenger "]
-#import wwwroot.app as application
+# uncomment this stuff to check passeger setup ok.
+#def application(environ, start_response):
+#	start_response('200 OK', [('Content-type', 'text/plain')])
+#	return ["Hello, world! Passenger "]
+from wwwroot.app import app as application
 
 # Uncomment next two lines to enable debugging
 from werkzeug.debug import DebuggedApplication
